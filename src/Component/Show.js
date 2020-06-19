d303365479a4dd58da4670b4f7a2ad8f73efea64
Erwin6997 <59776155+Erwin6997@users.noreply.github.com>
2020-06-19 23:35:50 +0100

    Update Show.js

--- src/Component/Show.js
+++ src/Component/Show.js
@@ -23,7 +23,7 @@ const Body = ({showFetch, eventFirst, eventSecond}) => {
                     <h3> {showFetch.routeSections[0].originationName}</h3>
                 </div>
                 <div>
-                    <img alt="image" className="logo-img" src="https://imageog.flaticon.com/icons/png/512/25/25426.png?size=1200x630f&pad=10,10,10,10&ext=png&bg=FFFFFFFF" ></img>
+                    <img alt="logo next" className="logo-img" src="https://imageog.flaticon.com/icons/png/512/25/25426.png?size=1200x630f&pad=10,10,10,10&ext=png&bg=FFFFFFFF" ></img>
                 </div>
                 <div className="blue">
                     <h3>End of the line:</h3>
